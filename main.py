6d559d77d122b52fcb6fbcdc1616635fa13cd6d2
moritasoshi <soshi.morita@rakus-partners.co.jp>
2021-03-31 09:40:48 +0900

    query-params

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from enum import Enum
-
+from typing import Optional
 from fastapi import FastAPI
+
+app = FastAPI()
 
 
 class ModelName(str, Enum):
@@ -9,12 +11,19 @@ class ModelName(str, Enum):
     lenet = "lenet"
 
 
-app = FastAPI()
+fake_item_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
 
 
 @app.get("/items/{item_id}")
-async def read_item(item_id: int):
+async def read_item(item_id: int, q: Optional[str] = None):
+    if q:
+        return {"item_id": item_id, "q": q}
     return {"item_id": item_id}
+
+
+@app.get("/items/")
+async def read_items(skip: int = 0, limit: int = 10):
+    return fake_item_db[skip: skip + limit]
 
 
 @app.get("/users/me")
